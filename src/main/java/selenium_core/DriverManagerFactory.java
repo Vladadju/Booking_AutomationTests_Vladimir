d4188b9215bbb538a04e285bf5df831e0a3f026f
vladimir <negovan.djurovic@zamia.be>
2022-06-22 14:34:49 +0200

    added package selenium core

---
+++ src/main/java/selenium_core/DriverManagerFactory.java
@@ -0,0 +1,24 @@
+package selenium_core;
+
+public class DriverManagerFactory {
+    public static DriverManager getDriverManager(String browser) {
+        DriverManager driverManager;
+
+        switch (browser) {
+            case "Chrome": {
+                driverManager = new ChromeDriverManager();
+            }
+            break;
+            case "Firefox": {
+                driverManager = new FirefoxDriverManager();
+
+            }
+            break;
+            default:
+                driverManager = new ChromeDriverManager();
+
+        }
+        return driverManager;
+    }
+
+}
